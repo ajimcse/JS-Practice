72dc1a871f997f1c861624e4a6b8ec67cf0691d7
ajimcse <mdajimsorkarcse40@gmail.com>
2026-06-27 23:25:31 +0600

    if else

---
+++ Multipale.js
@@ -0,0 +1,25 @@
+var salary =25000;
+var isBCS =true;
+var height =60;
+
+// if(salary > 20000 && height < 50){
+//     console.log('su patro.........')
+// }
+// else{
+//     console.log('onnno jayga jao')
+// }
+
+if(salary < 20000 || height > 70 ){
+    console.log('good Boy')
+}
+const price =3000;
+if(price >=5000){
+    const discount= price *10 / 100;
+    const payAmount= price -discount;
+    console.log(payAmount)
+}
+else if( price >= 3000){
+    const discount =price *5 /100;
+    const payAmount =price -discount;
+    console.log(payAmount)
+}
---
+++ if.js
@@ -0,0 +1,10 @@
+if(13 > 10){
+    console.log('kotha sotti')
+} 
+var biraniPrice =200;
+
+if(biraniPrice < 300){
+   console.log('birani khabo')
+} else{
+    console.log('singra khabo')
+}
