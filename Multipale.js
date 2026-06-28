0fb7a3bb2bc3deba59971bb910737007af3f9ad6
ajimcse <mdajimsorkarcse40@gmail.com>
2026-06-28 19:22:54 +0600

    if else practice

--- Multipale.js
+++ Multipale.js
@@ -9,17 +9,26 @@ var height =60;
 //     console.log('onnno jayga jao')
 // }
 
-if(salary < 20000 || height > 70 ){
-    console.log('good Boy')
+// if(salary < 20000 || height > 70 ){
+//     console.log('good Boy')
+// }
+// const price =3000;
+// if(price >=5000){
+//     const discount= price *10 / 100;
+//     const payAmount= price -discount;
+//     console.log(payAmount)
+// }
+// else if( price >= 3000){
+//     const discount =price *5 /100;
+//     const payAmount =price -discount;
+//     console.log(payAmount)
+// }
+const burgerPrice =500;
+
+if(burgerPrice > 500){
+    console.log('free coke')
+} else{
+    console.log('coke cost 30 tk')
 }
-const price =3000;
-if(price >=5000){
-    const discount= price *10 / 100;
-    const payAmount= price -discount;
-    console.log(payAmount)
-}
-else if( price >= 3000){
-    const discount =price *5 /100;
-    const payAmount =price -discount;
-    console.log(payAmount)
-}
+
+  
---
+++ if-else.js
@@ -0,0 +1,78 @@
+// const age =20;
+// const height =70;
+
+// if(age > 18 && height > 60){
+//     console.log('tmi vot dite parbe')
+// } else{
+//     console.log('tmi vot dite parbe nah')
+// }
+
+// const math =90;
+// const english =87;
+
+// if(math >  80 || english > 85){
+//     console.log('tmi porte parbe')
+// } else{
+//     console.log( 'tmi porte parbe nah')
+// }
+// const GPA = 5;
+// const monthleIncome=80000;
+
+// if(GPA > 5 || monthleIncome >10000){
+//     console.log('tmi free porte parbe')
+// } else{
+//     console.log('tk diye tmr porte hobe')
+// }
+// const basayDim =12;
+// const murgiNei = false;
+// if(basayDim < 10 || murgiNei == true){
+//    console.log('paruti kheye thak')
+// } else{
+//     console.log('na kheye thak')
+// }
+// const price =4500;
+// if( price < 3500 ){
+//     const discount = price *5 /100;
+//     const payAmount =  price -discount
+//     console.log(payAmount)
+// }
+//  else if( price > 6000){
+//     const discount = price *15 /100;
+//     const payAmount = price -discount;
+//     console.log(payAmount)
+
+// }
+//  else if( price >= 4500 ){
+//     const discount = price * 15 /100;
+//     const payAmount =price -discount;
+//     console.log(payAmount)
+// }
+// const age =62;
+
+// if(age < 12){
+//     console.log('tmi free khabe')
+// } 
+// else if( age <= 60){
+//    console.log(' 50% tk dibe') 
+// } else{
+//     console.log('bakider puro tk dibe')
+// }
+
+// const marks =90;
+
+// if(marks < 50){
+//     console.log('tmi fall')
+// } else if(marks >= 50 && marks <=80){
+//     console.log('tmi pass')
+// } else{
+//     console.log('tmi A + paico')
+// }
+
+// const books = 200;
+// if(books < 100){
+//     console.log('small books')
+// } else if( books >= 100 && books <=500){
+//     console.log('Miz book')
+// } else{
+//     console.log('heart book ')
+// }
