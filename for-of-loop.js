addaca035c43019a0a7b8c77dffa3785da5f3e30
ajimcse <mdajimsorkarcse40@gmail.com>
2026-06-29 23:55:19 +0600

    while loop

--- for-of-loop.js
+++ for-of-loop.js
@@ -1,7 +1,6 @@
-const number =[32,43,54,75,86,34]
-console.log(number)
+// const number =[32,43,54,75,86,34]
+// console.log(number)
 
 
-for (const num of number){
-    console.log(num)
-}
+// for (const num of number){
+//     console.log(num)
--- while.js
+++ while.js
@@ -1,5 +1,26 @@
-let num= 0;
-while(num < 5){
-       num = num + 1
-       console.log('while loop', num)
+// let num= 0;
+// while(num < 5){
+//        num = num + 1
+//        console.log('while loop', num)
+// }
+
+// let num=1
+// while(num <= 10){
+//     console.log(num);
+//     num++;
+// }
+
+// let num=50
+// while(num <= 100){
+//     console.log(num);
+//     num++;
+// }
+
+let num =1;
+let sum =0
+while(num <=10 ){
+    console.log(num)
+    sum = sum + num;
+    console.log(sum)
+    num++;
 }
