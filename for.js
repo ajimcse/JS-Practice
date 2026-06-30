0d3b444d6d25d330911593fbcfc661efe39fb65d
ajimcse <mdajimsorkarcse40@gmail.com>
2026-06-30 22:37:03 +0600

    loop practice

--- for.js
+++ for.js
@@ -17,7 +17,127 @@
 //     // console.log(i)
 // }
 // console.log(sum)
- 
- for(let i =10; i >=0; i--){
-      console.log(i)
- }
+
+//  for(let i =10; i >=0; i--){
+//       console.log(i)
+//  }
+
+// break 
+// for(let i=0; i < 15; i++ ){
+//    console.log(i);
+//    if( i >= 5){
+//      break
+//    }
+// }
+
+// continue 
+
+// for(let i =1; i <=10; i++){
+//       if(i %2 ===1){
+//           continue
+//       }
+//       console.log(i)
+// }
+
+// for(let i =0; i <=200; i++){
+//      if(i >100){
+//           break
+//      }
+//      console.log(i)
+// }
+
+
+// for(let i =0; i <=100 ; i++){
+//       if(i  % 2==0){
+//           continue
+//       }
+//       console.log(i)
+// }
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i *i)
+// }
+
+//  let sum =0
+// for(let i =1; i<=50; i++){
+//     sum = sum + i;
+// }
+//     console.log(sum)
+
+
+// for (let i = 1; i <= 30; i++) {
+//     if(i % 3 ===0){
+//      console.log(i)
+//     }
+// }
+
+// for(let i=20; i >=0; i--){
+//      console.log(i)
+// }
+
+//   let sum =0
+// for (let i = 1; i <= 50; i++) {
+//    if(i % 5===0){
+//      sum = sum +i
+//    }
+// }
+//    console.log(sum)
+
+//  let count=0
+// for (let i = 1; i <= 100; i++){
+//      if(i % 7===0){
+//       count++;
+
+//      }
+
+// }
+// console.log(count)
+
+// let sum =0;
+
+// for (let i = 0; i <= 100; i++) {
+//    if(i % 5 ===0 && i % 3===0){
+//       sum = sum + i;
+//    }
+// }
+// console.log(sum)
+
+//  let count =0
+// for (let i = 1; i <= 100; i++) {
+//      if (i % 4 === 0) {
+//      count++;
+
+//      }
+// }
+// console.log(count)
+
+//  let sum=0
+// for (let i = 1; i<= 100; i++) {
+//    if(i % 3 !==0 && i % 5 !==0){
+//        sum = sum + i
+//    }
+// }
+// console.log(sum)
+
+
+// let count =0;
+// for(let i =0; i <=100; i++){
+//      if(i % 2 !==0 && i % 3 !==0 && i % 5 !==0){
+//           count++;
+//      }
+// }
+// console.log(count)
+
+
+// let count =0;
+// for(let i =0; i <=100; i++){
+//      if(i % 2 ==0 || i % 3 ==0 && i % 5 !==0){
+//           count++;
+//      }
+// }
+// console.log(count)
+for (let i = 0; i <= 40; i++){
+     if( i % 2===1){
+          continue
+     }
+     console.log(i) 
+}
