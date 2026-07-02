127fcc7611dedc400925470bfc3bbc4ce9eff412
ajimcse <mdajimsorkarcse40@gmail.com>
2026-07-02 23:12:29 +0600

    function practice

--- function.js
+++ function.js
@@ -29,8 +29,87 @@ function differnce(num1, num2) {
 // console.log(result)
 
 
-function tenTimes (number){
-    const result= number *10;
+function tenTimes(number) {
+    const result = number * 10;
     return result;
 }
+// console.log(result)
+
+function doMath(num1, num2) {
+    const sum = num1 + num2;
+    const diff = num1 - num2;
+    const multiply = num1 * num2;
+    const result = multiply / 2;
+    return result;
+}
+// const result = doMath(11, 2);
+// console.log(result)
+
+function isEven(number) {
+    if (number % 2 == 0) {
+        return true
+    } else {
+        return false;
+    }
+}
+// const result =isEven(11);
+// console.log(result)
+
+
+function isEvenAndOddSize(str) {
+    const size = str.length;
+    console.log(size)
+    if (size % 2 === 0) {
+        console.log('even')
+
+        return true;
+    }
+    else {
+        console.log('odd')
+        return false;
+
+    }
+}
+// isEvenAndOddSize('Dhaka')
+// isEvenAndOddSize('faka')
+
+
+function double(number, doDouble) {
+    if (doDouble === true) {
+        const result = number * 2;
+        return result;
+    } else {
+        const result = number * 3
+        return result;
+    }
+}
+// console.log(double(5,true))
+// console.log(double(5,false))
+
+function numberOfElements(numbers) {
+    const len = numbers.length;
+    return len;
+
+}
+// const result =numberOfElements([12,32,43,656,767,343,323])
+// console.log(result)
+
+function getAge(person) {
+    const age = person.age;
+    return age;
+}
+// const result =getAge({
+//     name:'ajim',
+//     age:25
+// })
+// console.log(result)
+
+function sumOfNumber(numbers) {
+    let sum = 0;
+    for (const num of numbers) {
+        sum = sum + num
+    }
+    return sum
+}
+const result = sumOfNumber([12, 434, 535, 5656, 323, 1]);
 console.log(result)
