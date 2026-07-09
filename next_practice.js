9e25024e3c2d9b674db6abaf2ae0360b0936bbc5
ajimcse <mdajimsorkarcse40@gmail.com>
2026-07-09 12:38:33 +0600

    countboolean

--- next_practice.js
+++ next_practice.js
@@ -59,23 +59,71 @@
 // console.log(resul)
 
 
-const products = [
-    { name: "Laptop", price: 50000 },
-    { name: "Mouse", price: 800 },
-    { name: "Keyboard", price: 1500 },
-    { name: "Phone", price: 65000 }
-];
-function productMinPrice(products) {
-     let pp=[]
-    for (const product of products) {
-        const productPrice = product.price
-        if (productPrice < 1000) {
-            pp.push(product)
+// const products = [
+//     { name: "Laptop", price: 50000 },
+//     { name: "Mouse", price: 800 },
+//     { name: "Keyboard", price: 1500 },
+//     { name: "Phone", price: 65000 }
+// ];
+// function productMinPrice(products) {
+//      let pp=products[0]
+//     for (const product of products) {
 
-        }
+//         if ( product.price > pp.price) {
+//            pp =product
+
+//         }
+//     }
+//     return pp
+
+// }
+// const resul = productMinPrice(products);
+// console.log(resul)
+
+// function maxNumber(array) {
+
+//     if (!Array.isArray(array)) {
+//         return 'this is a not an array'
+//     }
+//     let max = array[0];
+//     for (const arr of array) {
+//         if (arr > max) {
+//             max = arr
+//         }
+//     }
+//     return max
+// }
+// const numbers = [12, 43, 53, 23, 8, 6, 199, 23, 54, 99, 121, 4, 2,]
+// const resul = maxNumber(numbers);
+// console.log(resul)
+
+
+// function showInfo(info) {
+//     if (typeof info !== 'string' || !info.includes("@")) {
+//         return 'Invalid Email'
+//     }
+//     const parts = info.split("@");
+//     const resul = parts[0] + " sent you an email from " + parts[1]
+//     return resul
+// }
+
+// const person = 'ajimsorkar@gmail.com'
+// const resul = showInfo(person);
+// console.log(resul)
+
+
+function countBoolean(array){
+    if(!Array.isArray (array)){
+        return 'invalid Array'
     }
-    return pp
-
+    let booleanCount=0
+   for(const arr of array){
+      if(typeof arr ==='boolean'){
+        booleanCount++
+      }
+   }
+   return booleanCount
 }
-const resul = productMinPrice(products);
+const  array =[12,43,true,443,'ajim', false, 'Pabna',true,false];
+const resul =countBoolean(array)
 console.log(resul)
